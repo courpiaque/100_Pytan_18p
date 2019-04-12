3a856207e841cac5105c391d838ea35bad966a28
Bartosz Kamiński <bkaminski98@outlook.com>
2019-04-12 08:16:25 -0700

    details - rate us, counter fixed, portrait

--- StoPyta_JedenOsiemL/StoPyta_JedenOsiemL/HardPage.xaml.cs
+++ StoPyta_JedenOsiemL/StoPyta_JedenOsiemL/HardPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,11 +15,14 @@ namespace StoPyta_JedenOsiemL
 	{
         private HardQuestions questions;
         private int x = 1;
+        private static readonly string dir = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "Rate");
+        DirectoryInfo d;
 
         public HardPage ()
 		{
 			InitializeComponent ();
             questions = new HardQuestions();
+            d = new DirectoryInfo(dir);
         }
 
         private void Button_Clicked(object sender, EventArgs e)
@@ -26,8 +30,24 @@ namespace StoPyta_JedenOsiemL
             label.Text = questions.GetRandomQuestion();
             if (x % 10 == 0) DependencyService.Get<IAdInterstitial>().ShowAd();
             if (x > 75) x = 1;
+            if (x == 75 && !d.Exists)
+            {
+                Directory.CreateDirectory(dir);
+                d = new DirectoryInfo(dir);
+                RateUs();
+            } 
             counter.Text = x + "/75";
             x++;
         }
+
+        public async Task RateUs()
+        {
+            var action = await App.Current.MainPage.DisplayAlert("Oceń nas!", "Podziel się z nami swoją oceną naszej aplikacji.", "Wystaw ocenę", "Nie, dziękuję");
+            if (action)
+            {
+                Device.OpenUri(new Uri("https://play.google.com/store/apps/details?id=com.Billog.Studio.StoPyta_JedenOsiemL"));
+            }
+        }
+
     }
 }
